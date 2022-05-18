e707b9e4071f3a03a6dc38747bcf12f0f3f970a2
Kristina <kasyankn@gmail.com>
2022-05-18 17:00:29 +0300

    Задание по спискам

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,5 +1,69 @@
 import { IItem } from './index';
+import { useState } from 'react';
 
 export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
-    return <div></div>;
+    const [id, setId] = useState(-1);
+    const [name, setName] = useState('');
+
+    if (props.sorting === 'DESC') {
+        props.initialData.sort((a, b) => b.id - a.id);
+    } else if (props.sorting === 'ASC') {
+        props.initialData.sort((a, b) => a.id - b.id);
+    }
+
+    // return (
+    //     <div>
+    //         {props.initialData.map((user) =>
+    //             user.id != id ? (
+    //                 <div onClick={() => setId(user.id)} key={user.id}>
+    //                     {user.name}
+    //                 </div>
+    //             ) : (
+    //                 <input
+    //                     autoFocus={true}
+    //                     onChange={(e) => setName(e.currentTarget.value)}
+    //                     key={user.id}
+    //                     defaultValue={user.name}
+    //                     onKeyDown={(e) => {
+    //                         if (e.key == 'Escape') {
+    //                             setId(0);
+    //                         }
+    //                         if (e.key == 'Enter') {
+    //                             user.name = name;
+    //                             setId(0);
+    //                         }
+    //                     }}
+    //                 />
+    //             ),
+    //         )}
+    //     </div>
+    // );
+    return (
+        <div>
+            {props.initialData.map((item) => {
+                if (item.id != id) {
+                    return (
+                        <div onClick={(e) => setId(item.id)} key={item.id}>
+                            {item.name}
+                        </div>
+                    );
+                } else
+                    return (
+                        <input
+                            autoFocus={true}
+                            onChange={(e) => setName(e.target.value)}
+                            onKeyDown={(e) => {
+                                if (e.key == 'Escape') setId(0);
+                                if (e.key == 'Enter') {
+                                    item.name = name;
+                                    setId(0);
+                                }
+                            }}
+                            key={item.id}
+                            defaultValue={item.name}
+                        ></input>
+                    );
+            })}
+        </div>
+    );
 }
